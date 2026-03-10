0f55b907cb0dea4d6c0752c90d32dd238ccd8205
pavankalyanvarmavegesana <vegesanapavankalyanvarma@gmail.com>
2026-03-10 22:32:20 +0530

    Day _1

---
+++ Doctor_Consultations/src/main/java/com/example/Doctor_Consultations/DoctorConsultationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Doctor_Consultations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorConsultationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorConsultationsApplication.class, args);
+	}
+
+}
